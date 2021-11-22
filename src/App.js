a04cda4d6ade153f49c7906403009772f450814f
Marcoarielf <64933125+Marcoarielf@users.noreply.github.com>
2021-11-22 12:34:13 -0300

    updaing firebase dependency and fixing styles

--- src/App.js
+++ src/App.js
@@ -1,77 +1,81 @@
-import React, { useEffect, useState, useContext } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './Component/Nav/nav'
-import FiltroHome from './Component/Filtros/filtroHome'
-import CartList from './Component/CartList/cartList'
-import ProductDetail from './Component/productDetail/productDetail'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import {CartProvider} from './Context/CartContext'
-import Loading from './images/loading.gif'
-import Cart from './Component/Cart/cart'
-import {getFirestore} from './Firebase/index'
-import FormBuy from './Component/Form_buy/formBuy'
-import Footer from './Component/footer/footer'
+import React, { useEffect, useState, useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Component/Nav/nav";
+import FiltroHome from "./Component/Filtros/filtroHome";
+import CartList from "./Component/CartList/cartList";
+import ProductDetail from "./Component/productDetail/productDetail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { CartProvider } from "./Context/CartContext";
+import Loading from "./images/loading.gif";
+import Cart from "./Component/Cart/cart";
+import { getFirestore } from "./Firebase/index";
+import FormBuy from "./Component/Form_buy/formBuy";
+import Footer from "./Component/footer/footer";
 
 function App() {
-
-  const [data, setData] = useState([]); 
+  const [data, setData] = useState([]);
 
   const [theme, settheme] = useState(true);
-  const [loading,setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  useEffect( () => { 
-        const db = getFirestore();
-        const itemColection = db.collection('items');
+  useEffect(() => {
+    const db = getFirestore();
+    const itemColection = db.collection("items");
 
-    itemColection.get()
-    .then((response) => {
-        if(response.size == 0 ){
+    itemColection
+      .get()
+      .then((response) => {
+        if (response.size == 0) {
           console.log("NO HAY DATA");
+        } else {
+          setData(
+            response.docs.map((doc) => {
+              return { id: doc.id, ...doc.data() };
+            })
+          );
         }
-        else{
-          setData(response.docs.map( (doc) => {return({id:doc.id,...doc.data()});
-        }))
-        }
-     })
-     .catch ( (error) => {console.log("Algo fallo", error);} )
-     .finally( () => {
-        setLoading(false)
-     } )
-      
+      })
+      .catch((error) => {
+        console.log("Algo fallo", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
 
-  },[]);
+    console.log(data);
+  }, []);
 
-  function changeTheme(){
-    
-    if(theme){console.log(theme); settheme(false)}
-    else{settheme(true)}
-    
-  } 
+  function changeTheme() {
+    if (theme) {
+      console.log(theme);
+      settheme(false);
+    } else {
+      settheme(true);
+    }
+  }
 
-if(loading){
-  return <div className="container-loading"><img src={Loading} className="loading"></img></div>
-}else{
-
+  if (loading) {
+    return (
+      <div className="container-loading">
+        <img src={Loading} className="loading"></img>
+      </div>
+    );
+  } else {
     return (
       <div className={theme ? "App" : "App-dark"}>
         <Router>
           <CartProvider>
-              <NavBar changeTheme={changeTheme}/>
-                <switch>
-                  <Route exact path="/">
-                    <FiltroHome updateData={setData} data={data}/>
-                    < CartList data={data}/>
-                  </Route>
-                  <Route exact path="/item/:id" component={ProductDetail}/>
-                  <Route exact path="/cart" component={Cart}/>
-                  <Route exact path="/cart/formBuy" component={FormBuy}/>
-                </switch>
+            <NavBar changeTheme={changeTheme} />
+            <switch>
+              <Route exact path="/">
+                <FiltroHome updateData={setData} data={data} />
+                <CartList data={data} />
+              </Route>
+              <Route exact path="/item/:id" component={ProductDetail} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/cart/formBuy" component={FormBuy} />
+            </switch>
           </CartProvider>
           <Footer />
         </Router>
--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,23 +1,24 @@
-  import * as firebase from 'firebase/app';
-    // import firebase from 'firebase/app';
-  import 'firebase/firestore';
-  
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyBFjj7ujURwiUmHA6T4vzNLiu-QiGls5fE",
-    authDomain: "ecommerce-ch-85557.firebaseapp.com",
-    databaseURL: "https://ecommerce-ch-85557.firebaseio.com",
-    projectId: "ecommerce-ch-85557",
-    storageBucket: "ecommerce-ch-85557.appspot.com",
-    messagingSenderId: "681222300954",
-    appId: "1:681222300954:web:75eed82fd231dbf54af8a3"
-});
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFjj7ujURwiUmHA6T4vzNLiu-QiGls5fE",
+  authDomain: "ecommerce-ch-85557.firebaseapp.com",
+  databaseURL: "https://ecommerce-ch-85557.firebaseio.com",
+  projectId: "ecommerce-ch-85557",
+  storageBucket: "ecommerce-ch-85557.appspot.com",
+  messagingSenderId: "681222300954",
+  appId: "1:681222300954:web:75eed82fd231dbf54af8a3",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export function getFirebase() {
-    return app;
-
+  return app;
 }
 
 export function getFirestore() {
-    return firebase.firestore(app);
-};
-
+  return firebase.firestore(app);
+}
